bfcab54ce131bd702b4fb973fbacf9d57073a892
SecondBook5 <ajbook12@gmail.com>
2026-03-02 16:47:42 -0500

    tangram runs

--- stagebridge/io/__init__.py
+++ stagebridge/io/__init__.py
@@ -8,12 +8,26 @@ from .hlca import (
     load_hlca_reference,
     map_to_hlca_latent,
 )
+from .tangram import (
+    TangramMappingResult,
+    run_tangram_hlca_projection,
+)
+from .interim_build import (
+    InterimBuildResult,
+    build_snrna_interim_anndata,
+    build_spatial_interim_anndata,
+)
 
 __all__ = [
     "HLCA_FULL_FILENAME",
     "HLCA_FULL_URL",
     "HLCAReference",
+    "InterimBuildResult",
+    "TangramMappingResult",
+    "build_snrna_interim_anndata",
+    "build_spatial_interim_anndata",
     "download_hlca_reference",
     "load_hlca_reference",
     "map_to_hlca_latent",
+    "run_tangram_hlca_projection",
 ]
