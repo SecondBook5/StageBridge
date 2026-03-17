20c955eda9cb3cbd448311c0d1303b12cceb4dbf
SecondBook5 <ajbook12@gmail.com>
2026-03-17 18:56:02 -0400

    ready for HPC

---
+++ stagebridge/config/__init__.py
@@ -0,0 +1,5 @@
+"""StageBridge configuration module."""
+
+from stagebridge.config.paths import PathConfig, get_paths, detect_environment, print_status
+
+__all__ = ["PathConfig", "get_paths", "detect_environment", "print_status"]
